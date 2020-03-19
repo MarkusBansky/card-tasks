f781026f50f71a6d4d4f54cf16b891b7bc6ebf1e
MarkusBansky <MarkusBansky@users.noreply.github.com>
2020-03-19 21:23:58 +0000

    Added hidden tiles

--- src/pages/Display.tsx
+++ src/pages/Display.tsx
@@ -9,13 +9,15 @@ import Task from "../models/Task";
 
 interface DisplayState {
     tasks: Task[];
+    revealed: boolean;
     checkedIndex: number;
 }
 
 class Display extends React.Component<{}, DisplayState> {
     state: DisplayState = {
         tasks: [],
-        checkedIndex: 0
+        checkedIndex: 0,
+        revealed: false
     };
 
     componentDidMount(): void {
@@ -26,17 +28,42 @@ class Display extends React.Component<{}, DisplayState> {
         this.setState({...this.state, tasks: shuffledData});
     }
 
+    flipCard = (index: number) => {
+        this.setState({
+            ...this.state,
+            checkedIndex: this.state.checkedIndex + 1,
+            revealed: false
+        });
+    };
+
+    revealCard = () => {
+        this.setState({
+            ...this.state,
+            revealed: true
+        });
+    };
+
     renderCard(task: Task, index: number, hasNext: boolean) {
+        const {revealed, checkedIndex} = this.state;
+
+        if (task.hidden && !revealed && checkedIndex === index) {
+            return (
+                <div className="content">
+                    <label>CLICK TO REVEAL</label>
+                </div>
+            );
+        }
+
         return (
             <div className="content">
                 <h1>{task.value}</h1>
-                <span className="card-explanatory">Click the card to flip</span>
+                {/*<span className="card-explanatory">Click the card to flip</span>*/}
             </div>
         );
     }
 
     renderStacks() {
-        const {checkedIndex, tasks} = this.state;
+        const {checkedIndex, tasks, revealed} = this.state;
 
         if (tasks.length === 0) {
             return <Alert variant={'primary'}>No tasks in this list</Alert>;
@@ -53,7 +80,9 @@ class Display extends React.Component<{}, DisplayState> {
                                 type={'radio'}
                                 checked={checkedIndex === index}
                             />
-                            <div className={'stack-card'} onClick={() => this.setState({...this.state, checkedIndex: this.state.checkedIndex + 1})}>
+                            <div
+                                className={`stack-card ${item.hidden && !revealed && index === checkedIndex && 'stack-card-hidden'}`}
+                                onClick={() => item.hidden && !revealed ? this.revealCard() : this.flipCard(index)}>
                                 {this.renderCard(item, index, index < tasks.length)}
                             </div>
                         </>
